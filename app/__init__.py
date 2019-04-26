a2076b6ab5c7fffddfd103cac6c0f58005d23c9d
Nickoy Morrison <nickoy.morrison@mymona.uwi.edu>
2019-04-26 20:01:53 +0000

    This is our first commit

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
+import os
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+bcrypt = Bcrypt(app)
+app.config['SECRET_KEY'] = "this is a super secure key"  # you should make this more random and unique
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ospppqppphtysf:13aebfba2b841c15dcf9d60f50262e629d09f5c8d503fb4faf1142ce289b13d8@ec2-54-235-193-34.compute-1.amazonaws.com:5432/d99r9bmdq71p21"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project:password123@localhost/project"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
+
+UPLOAD_FOLDER ='./app/static/uploads'
+PROFILE_IMG_UPLOAD_FOLDER = os.path.join("static/uploads", "profile_photos")
+POST_IMG_UPLOAD_FOLDER = os.path.join("static/uploads", "posts")
+
+db = SQLAlchemy(app)
+
+app.config.from_object(__name__)
+from app import views
